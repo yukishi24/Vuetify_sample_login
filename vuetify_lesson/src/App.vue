e3dda4a37d0c0f1f2cb8dc811de0818322ddb896
yukishi24 <yukishi24github@gmail.com>
2023-04-11 21:41:47 -0700

    create: login form

--- vuetify_lesson/src/App.vue
+++ vuetify_lesson/src/App.vue
@@ -1,60 +1,47 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+<v-app>
+  <v-card width="400px" class="mx-auto mt-5">
+    <v-card-title>
+      <h1 class="display-1">ログイン</h1>
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field 
+  prepend-icon="mdi-account-circle" 
+  label="ユーザ名" 
+  v-model="name" 
+/>
+<v-text-field 
+  v-bind:type="showPassword ? 'text' : 'password'"
+  @click:append="showPassword = !showPassword"
+  prepend-icon="mdi-lock" 
+  v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+  label="パスワード"
+  v-model="password"
+/>  
+  <v-card-actions>
+    <v-btn class="info" @click="submit">ログイン</v-btn> 
+  </v-card-actions>
+</v-form>
+    </v-card-text>
+  </v-card>
+</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
+  data(){
+    return {
+      showPassword : false,
+      name:'',
+      password:'',
+    }
   },
-
-  data: () => ({
-    //
-  }),
+  methods:{
+    submit(){
+      console.log(this.name,this.password)
+    }
+  }
 };
 </script>
